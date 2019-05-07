fa40b9feb65559eecf030bbec08fcfcc428a1149
carmen-tm <carmentorremolina@gmail.com>
2019-05-07 20:56:38 +0200

    Create MainPreview component

---
+++ src/components/MainPreview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainPreview extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Main preview</h1>
+			</div>
+		);
+	}
+}
+
+export default MainPreview;
