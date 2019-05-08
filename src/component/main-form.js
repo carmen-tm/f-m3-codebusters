3a5f626a82b7c8f9ef2427ff9f8f158fdb01ab1d
sarayfg <sarayfernandezgarcia@gmail.com>
2019-05-08 20:21:02 +0200

    add main-form component

---
+++ src/component/main-form.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class MainForm extends React.Component {
+    render () {
+        return (
+        <form action="" method="post">
+        {/* Este es el componente del main form principal
+        Está en blanco. En él se incluirán 3 componentes más pequeños, los fieldset del formulario, que a su vez contiene dos componentes mas pequeños, el legend y su contenido */}
+            
+          </form>
+        );
+    }
+}
+
+export default MainForm;
