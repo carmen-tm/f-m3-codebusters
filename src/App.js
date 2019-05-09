bef230759997af5620ae7ed94a46f8f566460b9c
sarayfg <sarayfernandezgarcia@gmail.com>
2019-05-09 20:26:20 +0200

    get object

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import React from 'react';
 import MainForm from './components/main-form';
 import MainPage from './components/MainPage.js';
 import './stylesheets/App.scss';
+import { userInfo } from 'os';
+
+
+let userInfo = {
+  'palette': '', 
+  'name': '',
+  'job': '',
+  'phone': '',
+  'email': '',
+  'linkedin': '',
+  'github': '',
+  'photo': '',
+  'miniature': '',
+};
 
 
 function App() {
+
   return (
     <div className="App">
       <MainPage />
