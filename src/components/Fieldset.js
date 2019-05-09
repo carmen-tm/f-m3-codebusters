30491ccca4474129993c741cbe9114990d75b118
valerialp <valeria.lorentep@gmail.com>
2019-05-09 20:21:04 +0200

    Create Fieldset component

---
+++ src/components/Fieldset.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Legend from './Legend';
+import Panel from './Panel';
+
+class Fieldset extends React.Component {
+    render() {
+        return (
+            <fieldset className="fieldset-item">
+                <Legend />
+                <Panel />
+            </fieldset>
+        )
+    }
+}
+
+export default Fieldset;
