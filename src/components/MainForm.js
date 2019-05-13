bb00399e1eb7ea3828b317d7df35f8a9be52cd93
carmen-tm <carmentorremolina@gmail.com>
2019-05-13 19:43:39 +0200

    Comment error

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -1,24 +1,24 @@
-import React from "react";
-import Fieldset from "./Fieldset";
-import PanelColors from "./PanelColors";
-import PanelFill from "./PanelFill";
-import PanelShare from "./PanelShare";
+import React from 'react';
+import Fieldset from './Fieldset';
+import PanelColors from './PanelColors';
+import PanelFill from './PanelFill';
+import PanelShare from './PanelShare';
 
 class MainForm extends React.Component {
-  render() {
-    return (
-      <form action="" method="post">
-        <Fieldset 
-          title="Design"
-          classIcon="far fa-object-ungroup"
-          btnId="ButtonDesign"
-          arrowId="arrow-design"
-          arrow="up"
-        >
-          <PanelColors />
-        </Fieldset>
+	render() {
+		return (
+			<form action="" method="post">
+				<Fieldset
+					title="Design"
+					classIcon="far fa-object-ungroup"
+					btnId="ButtonDesign"
+					arrowId="arrow-design"
+					arrow="up"
+				>
+					<PanelColors />
+				</Fieldset>
 
-        <Fieldset
+				{/* <Fieldset
           title="Fill"
           classIcon="far fa-keyboard"
           btnId="ButtonFill"
@@ -26,20 +26,20 @@ class MainForm extends React.Component {
           arrow="down"        
         >
           <PanelFill />
-        </Fieldset>  
+        </Fieldset>   */}
 
-        <Fieldset
-          title="Share"
-          classIcon="fas fa-share-alt"
-          btnId="ButtonShare"
-          arrowId="arrow-share"
-          arrow="down"
-        >
-          <PanelShare />
-        </Fieldset>
-      </form>
-    );
-  }
+				<Fieldset
+					title="Share"
+					classIcon="fas fa-share-alt"
+					btnId="ButtonShare"
+					arrowId="arrow-share"
+					arrow="down"
+				>
+					<PanelShare />
+				</Fieldset>
+			</form>
+		);
+	}
 }
 
 export default MainForm;
