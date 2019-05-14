d89f95e289fafbc333cab23d06c2588ad2e4b3e9
sarayfg <sarayfernandezgarcia@gmail.com>
2019-05-14 18:56:58 +0200

    create class

--- src/components/button.js
+++ src/components/button.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Button extends React.Component {
     render() {
         return(
-            <button type="button">{this.props.textButton}</button>
+            <button type="button" onClick={this.props.BtnClick} className={this.props.btnClass}>{this.props.textBtn}</button>
         )
     }
 }
