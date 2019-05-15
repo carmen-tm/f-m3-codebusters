0d9143c3d39dcac8cd92ac9ba8be557909234342
carmen-tm <carmentorremolina@gmail.com>
2019-05-15 12:17:25 +0200

    Refactor Fieldset.js with destructuring

--- src/components/Fieldset.js
+++ src/components/Fieldset.js
@@ -4,16 +4,17 @@ import Panel from './Panel';
 
 class Fieldset extends React.Component {
 	render() {
+		const { title, classIcon, btnId, arrowId, arrow, children } = this.props;
 		return (
 			<fieldset className="fieldset-item">
 				<Legend
-					title={this.props.title}
-					classIcon={this.props.classIcon}
-					btnId={this.props.btnId}
-					arrowId={this.props.arrowId}
-					arrow={this.props.arrow}
+					title={title}
+					classIcon={classIcon}
+					btnId={btnId}
+					arrowId={arrowId}
+					arrow={arrow}
 				/>
-				<Panel children={this.props.children} />
+				<Panel children={children} />
 			</fieldset>
 		);
 	}
