c4ed3c04fc2763d609bc66bc55025230c8e179c2
carmen-tm <carmentorremolina@gmail.com>
2019-05-15 12:19:27 +0200

    Refactor Legen.js with destructuring

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,37 +1,37 @@
 import React from 'react';
 
-
 class Legend extends React.Component {
-    constructor(props){
-        super(props);
-        this.createIconClass=this.createIconClass.bind(this);
-        this.createArrowClass=this.createArrowClass.bind(this);
-    }
-    
-    createIconClass(){
-        let classIcon = 'icon icon--fieldset ';
-        classIcon += this.props.classIcon
-        return classIcon
-    }
-   
-    createArrowClass(){
-        let arrowClass = 'arrow fas fa-chevron-';
-        arrowClass += this.props.arrow
-        return arrowClass
-    }
-    render() {
-        return (
-            <section className="fieldset__title__container">
-                <div className="icon-legend__container">
-                    <i className={this.createIconClass()}></i>
-                    <legend className="fieldset__title">{this.props.title}</legend>
-                </div>
-                <button type="button" className="btn--collapsable" id={this.props.btnId}>
-                    <i id={this.props.arrowId} className={this.createArrowClass()}></i>
-                </button >
-            </section >
-        )
-    }
+	constructor(props) {
+		super(props);
+		this.createIconClass = this.createIconClass.bind(this);
+		this.createArrowClass = this.createArrowClass.bind(this);
+	}
+
+	createIconClass() {
+		let classIcon = 'icon icon--fieldset ';
+		classIcon += this.props.classIcon;
+		return classIcon;
+	}
+
+	createArrowClass() {
+		let arrowClass = 'arrow fas fa-chevron-';
+		arrowClass += this.props.arrow;
+		return arrowClass;
+	}
+	render() {
+		const { title, btnId, arrwoId } = this.props;
+		return (
+			<section className="fieldset__title__container">
+				<div className="icon-legend__container">
+					<i className={this.createIconClass()} />
+					<legend className="fieldset__title">{title}</legend>
+				</div>
+				<button type="button" className="btn--collapsable" id={btnId}>
+					<i id={arrwoId} className={this.createArrowClass()} />
+				</button>
+			</section>
+		);
+	}
 }
 
 export default Legend;
