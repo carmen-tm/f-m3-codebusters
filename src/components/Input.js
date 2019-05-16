505264fc44675a347bf2563fc38b6668ef0c786c
carmen-tm <carmentorremolina@gmail.com>
2019-05-16 13:13:52 +0200

    Start the inputs as Controlled Components with state

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,32 @@
 import React from 'react';
 
 class Input extends React.Component {
+	constructor(props) {
+		super(props);
+		//Here we set the initial values (default texts rendered on the preview card)
+		this.state = {
+			userInfo: {
+				name: 'User',
+				job: 'User job',
+				phone: '',
+				email: '',
+				linkedin: '#',
+				github: '#',
+				photo: '',
+				miniature: ''
+			}
+		};
+		this.handleInputsOnChange = this.handleInputsOnChange.bind(this);
+	}
+
+	//Methods:
+	handleInputsOnChange(event) {
+		const key = event.currentTarget.name;
+		this.setState({
+			[key]: event.target.value
+		});
+	}
+
 	render() {
 		const {
 			divClassName,
@@ -19,7 +45,9 @@ class Input extends React.Component {
 				<label className={labelClassName} for={labelFor}>
 					{labelInnerText}
 				</label>
+
 				<input
+					onChange={this.handleInputsOnChange}
 					className={inputClassName}
 					type={inputType}
 					id={inputId}
