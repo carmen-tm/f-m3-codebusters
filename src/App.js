d8b899a99e8bcc2b6942553465d0883cc750613f
patrozada <patriciarozada@gmail.com>
2019-05-16 21:16:29 +0200

    Delete unnecessary lines

--- src/App.js
+++ src/App.js
@@ -3,17 +3,7 @@ import MainPage from './components/MainPage.js';
 import './stylesheets/App.scss';
 import { userInfo } from 'os';
 
-const infoUser ={
-  name: 'Patricia',
-  job: 'Front end developer',
-  phone: 697845963,
-  email:'',
-  linkedin: 'https://www.linkedin.com/in/sarayfernandezgarcia/',
-  github: '',
-  photo:'',
-  miniature:'',
-}
- 
+
 class App extends React.Component {
   constructor(props){
     super(props)
