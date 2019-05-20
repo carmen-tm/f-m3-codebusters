2c39434b65fad451aeb525c895076207e7f13c0b
Clara Miralles <clara.miralles.imperial@gmail.com>
2019-05-20 20:37:58 +0200

    Create link to CardGenerator.js at Landing.js

--- src/Landing.js
+++ src/Landing.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class Landing extends Component {
     render(){
-        return ('Soy la landing!!')
+        return (
+            <div>
+                <p>Soy la landing!! </p>
+                <Link to = "/card-generator">Empezar</Link>
+            </div>
+        )
     }
 }
 export default Landing
