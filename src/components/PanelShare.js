98a8c3d01c3e948b0b022bc275723c98c4344d93
sarayfg <sarayfernandezgarcia@gmail.com>
2019-05-23 20:26:47 +0200

    share link with twitter

--- src/components/PanelShare.js
+++ src/components/PanelShare.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import Twitter from './Twitter';
+
 
 class PanelShare extends React.Component{
   render(){
+    const { btnShare, urlCard, twitter} = this.props;
+    const shareTwitterLink= 'https://twitter.com/intent/tweet?text=My awesome profile card: '+ urlCard;
+    console.log(shareTwitterLink);
     return(
       <section id="section-share" className="section-share">
-        <button type="button" className="btn btn-share" onClick={this.props.btnShare}><i className="far fa-address-card icon-btn"></i> Create your card</button>
-        <p className="response">{this.props.urlCard}</p>
-        <a href={Twitter} className={this.props.twitter ? "twitter__link btn-twitter" : "twitter__link btn-twitter hidden"}>
+        <button type="button" className="btn btn-share" onClick={btnShare}><i className="far fa-address-card icon-btn"></i> Create your card</button>
+        <p className="response">{urlCard}</p>
+        <a href={shareTwitterLink} className={twitter ? "twitter__link btn-twitter" : "twitter__link btn-twitter hidden"} target="_blank" rel="noopener noreferrer">
             <i className="fab fa-twitter twitter-icon"></i>
             Share on Twitter
         </a>
--- src/components/Twitter.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Twitter = 'https://twitter.com/intent/tweet?text=My awesome profile card:';
-
-export default Twitter
